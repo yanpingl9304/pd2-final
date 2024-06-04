8c171b7931477772de3c1be6751ad26002a4440d
yanpingl9304 <85386893+yanpingl9304@users.noreply.github.com>
2024-06-04 12:55:51 +0800

    edit

--- src/main/java/com/testing/Main.java
+++ src/main/java/com/testing/Main.java
@@ -7,15 +7,14 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import javax.security.auth.login.LoginException;
 
 public class Main {
-    public static final String token = "MTI0NzEwMjI5NDU5MjMyNzcyMA.Gd3Ngn.KpOnG3dyM8MQrzKUWOD-yUfZ1xtcKeQVh9yWAg";
+    public static final String token = "MTI0NzEwMjI5NDU5MjMyNzcyMA.GjXg1q.UtM4UGAcXRZSANhJX1FrdWGscBLXo13ieWntQU";
 
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder.createDefault(token) // enable all default intents
-                  .enableIntents(GatewayIntent.GUILD_MEMBERS)// also enable privileged intent
-                  .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-                  .build();
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)// also enable privileged intent
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
         jda.addEventListener(new Listeners());
-        System.out.println("aspd;ams;lkdmas;lmd;alsmdl;kamdl;masl;fnmswlk;dngl;adsnmfg");
     }
 
 }
