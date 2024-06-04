0590b935bb8e67620efab09d732138170f7e2ad3
yanpingl9304 <85386893+yanpingl9304@users.noreply.github.com>
2024-06-04 12:35:10 +0800

    Update Main.java

--- src/main/java/com/testing/Main.java
+++ src/main/java/com/testing/Main.java
@@ -1,2 +1,20 @@
-package com.testing;public class Main {
+package com.testing;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static final String token = "MTI0NzEwMjI5NDU5MjMyNzcyMA.GjXg1q.UtM4UGAcXRZSANhJX1FrdWGscBLXo13ieWntQU";
+
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault(token) // enable all default intents
+                  .enableIntents(GatewayIntent.GUILD_MEMBERS)// also enable privileged intent
+                  .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                  .build();
+        jda.addEventListener(new Listeners());
+    }
+
 }
