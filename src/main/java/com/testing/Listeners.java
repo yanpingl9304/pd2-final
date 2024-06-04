0fc48e3318deeca773a968274b23f9e115cb391a
yanpingl9304 <85386893+yanpingl9304@users.noreply.github.com>
2024-06-04 12:46:28 +0800

    first commit

--- src/main/java/com/testing/Listeners.java
+++ src/main/java/com/testing/Listeners.java
@@ -1,2 +1,17 @@
-package com.testing;public class Listeners {
+package com.testing;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class Listeners extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        String message = event.getMessage().getContentRaw();
+        if(message.equalsIgnoreCase("how's the weather")) {
+            event.getChannel().sendMessage("SUNNYYY"+event.getAuthor().getName()).queue();
+        }
+    }
+
 }
