7a7ec946999f1987efa4b4e49b1e1144aba97987
yanpingl9304 <85386893+yanpingl9304@users.noreply.github.com>
2024-06-04 17:24:18 +0800

    "hide token in json"

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,5 @@ repositories {
 
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-beta.24")
+    implementation ("org.json:json:20210307")
 }
--- src/main/java/com/testing/Main.java
+++ src/main/java/com/testing/Main.java
@@ -3,18 +3,45 @@ package com.testing;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.json.JSONObject;
 
 import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
-    public static final String token = "MTI0NzEwMjI5NDU5MjMyNzcyMA.GjXg1q.UtM4UGAcXRZSANhJX1FrdWGscBLXo13ieWntQU";
+    private static final String token;
+
+    private static String readConfigFile(String fileName) {
+        StringBuilder sb = new StringBuilder();
+        try (InputStream is = Main.class.getClassLoader().getResourceAsStream(fileName);
+             BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    static {
+        String jsonString = readConfigFile("config.json");
+        JSONObject json = new JSONObject(jsonString);
+        token = json.getString("token");
+    }
+
 
     public static void main(String[] args) throws LoginException {
+
         JDA jda = JDABuilder.createDefault(token) // enable all default intents
                   .enableIntents(GatewayIntent.GUILD_MEMBERS)// also enable privileged intent
                   .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                   .build();
         jda.addEventListener(new Listeners());
-    }
 
+    }
 }
